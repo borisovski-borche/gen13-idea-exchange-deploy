b32129700fdebcae23dabb2fe93f3ff6f3f5d010
borisovski-borche <borisovski.borce@gmail.com>
2025-10-15 18:18:06 +0200

    updated helper message for root endpoing

--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `<h1 style="text-align: center; font-family: sans-serif">This is the root endpoint for the idea exchage api</h1>`;
   }
 }
